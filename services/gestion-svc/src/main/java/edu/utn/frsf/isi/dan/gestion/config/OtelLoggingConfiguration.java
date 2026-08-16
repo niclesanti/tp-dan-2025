ea41901c4574a6e8c3b4cd3a18c42d34a31a9e2b
Santiago Nicle <niclesantiago@gmail.com>
2026-08-16 12:47:50 -0300

    fix: Corrección ETAPA05 observabilidad y telemetría.

---
+++ services/gestion-svc/src/main/java/edu/utn/frsf/isi/dan/gestion/config/OtelLoggingConfiguration.java
@@ -0,0 +1,25 @@
+package edu.utn.frsf.isi.dan.gestion.config;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OtelLoggingConfiguration {
+
+    private final ObjectProvider<OpenTelemetry> openTelemetryProvider;
+
+    public OtelLoggingConfiguration(ObjectProvider<OpenTelemetry> openTelemetryProvider) {
+        this.openTelemetryProvider = openTelemetryProvider;
+    }
+
+    @PostConstruct
+    void installAppender() {
+        OpenTelemetry openTelemetry = openTelemetryProvider.getIfAvailable();
+        if (openTelemetry != null) {
+            OpenTelemetryAppender.install(openTelemetry);
+        }
+    }
+}
